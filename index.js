add6a3081e49079804566f9474b7311ca41ed59f
Vedant <vedantkhodwe1801@gmail.com>
2023-03-28 19:53:33 +0530

    thodasa js prompt and alert added

---
+++ index.js
@@ -0,0 +1,6 @@
+var yourName=prompt("What is your Name?");
+var firstletter = yourName.slice(0,1);
+var capitalised = firstletter.toUpperCase();
+var remaing = yourName.slice(1,yourName.length);
+var lowered = remaing.toLowerCase();
+alert('Hello '+capitalised+lowered+'!, welcome to my website.')
