87a214ba880113615078cb3dde24d15d38213aee
Vedant <vedantkhodwe1801@gmail.com>
2023-03-28 19:59:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var yourName=prompt("What is your Name?");
+var yourName=prompt("What is your Name? You can skip if you wish.");
 var firstletter = yourName.slice(0,1);
 var capitalised = firstletter.toUpperCase();
 var remaing = yourName.slice(1,yourName.length);
