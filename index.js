675f081353852930c3742e603cef0ac262fa6de4
Vedant <vedantkhodwe1801@gmail.com>
2023-03-30 09:35:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var yourName=prompt("What is your Name? You can skip if you wish.");
-var firstletter = yourName.slice(0,1);
-var capitalised = firstletter.toUpperCase();
-var remaing = yourName.slice(1,yourName.length);
-var lowered = remaing.toLowerCase();
-alert('Hello '+capitalised+lowered+'!, welcome to my website.')
+// var yourName=prompt("What is your Name? You can skip if you wish.");
+// var firstletter = yourName.slice(0,1);
+// var capitalised = firstletter.toUpperCase();
+// var remaing = yourName.slice(1,yourName.length);
+// var lowered = remaing.toLowerCase();
+// alert('Hello '+capitalised+lowered+'!, welcome to my website.')
