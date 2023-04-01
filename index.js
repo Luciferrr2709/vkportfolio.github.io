e4d2395195bb3f9c3096abb673b35d675f99044b
Vedant <vedantkhodwe1801@gmail.com>
2023-04-01 15:44:32 +0530

    javascript for typing animation

--- index.js
+++ index.js
@@ -4,3 +4,23 @@
 // var remaing = yourName.slice(1,yourName.length);
 // var lowered = remaing.toLowerCase();
 // alert('Hello '+capitalised+lowered+'!, welcome to my website.')
+
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() =>{
+        text.textContent = "Python Developer";
+    }, 0);
+    setTimeout(() =>{
+        text.textContent = "Web Developer";
+    }, 4000);
+    setTimeout(() =>{
+        text.textContent = "App Developer";
+    }, 8000);
+    setTimeout(() =>{
+        text.textContent = "Machine Learning Intermediate";
+    }, 12000);
+}
+
+textLoad();
+setInterval(textLoad, 12000);
