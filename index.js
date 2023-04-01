958100a0e9b08db6db2284c045b00d57ffdb7b89
Vedant <vedantkhodwe1801@gmail.com>
2023-04-01 15:51:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// var yourName=prompt("What is your Name? You can skip if you wish.");
-// var firstletter = yourName.slice(0,1);
-// var capitalised = firstletter.toUpperCase();
-// var remaing = yourName.slice(1,yourName.length);
-// var lowered = remaing.toLowerCase();
-// alert('Hello '+capitalised+lowered+'!, welcome to my website.')
 
 const text = document.querySelector(".sec-text");
 
