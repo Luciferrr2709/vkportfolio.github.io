28b05549d49a5ad47324bed3e554a69642af21dd
Vedant <vedantkhodwe1801@gmail.com>
2023-04-06 03:13:57 +0530

    new typing animation adding

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const text = document.querySelector(".sec-text");
-const textLoad1 = () => {
-    setTimeout(() =>{
-        text.textContent = "Python Developer";
-    }, 0);
-    setTimeout(() =>{
-        text.textContent = "Web Developer";
-    }, 4000);
-    setTimeout(() =>{
-        text.textContent = "App Developer";
-    }, 8000);
-    setTimeout(() =>{
-        text.textContent = "Machine Learning Intermediate";
-    }, 12000);
-}
-
-textLoad1();
-setInterval(textLoad1, 16000);
+var typed = new Typed(".autotype", {
+    strings:["Python Developer", "Web Developer", "Machine Learning Intermediate"],
+    typeSpeed:50,
+    backSpeed:10,
+    loop: true
+})
